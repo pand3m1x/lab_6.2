ca9cc90a21f46bc4568385852224081888d74f1a
TheRedQueen <pand3m1x@gmail.com>
2026-02-04 17:05:32 -0500

    ★ Added rest of code, added reflection questions to readme★

--- apiSimulator.js
+++ apiSimulator.js
@@ -49,3 +49,21 @@ async function handleData() {
 }
 
 handleData();
+
+getProductDetails()
+  .then((product) => {
+    console.log("Fetched product:", product);
+    return getProductReviews(product);
+  })
+  .then((reviews) => {
+    // Uncomment the line below if you want to throw an error
+    // throw new Error("Can't fetch reviews");
+    console.log("Fetched reviews:", reviews);
+    return getRelatedProducts(reviews);
+  })
+  .then((relatedProducts) => {
+    console.log("Fetched Related Products:", relatedProducts);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
---
+++ readme.md
@@ -0,0 +1,14 @@
+Why is it important to handle errors for each individual API call
+rather than just at the end of the promise chain?
+
+    Answer Here
+
+
+How does using custom error classes improve debugging and error identification?
+
+    Answer here
+
+
+When might a retry mechanism be more effective than an immediate failure response?
+
+    Answer Here
